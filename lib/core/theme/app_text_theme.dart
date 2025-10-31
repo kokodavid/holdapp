d610525cf7463d6028246d30060c11b2e22e1d64
walterLugalia <kifwolow@gmail.com>
2025-10-31 05:12:23 +0300

    feat: add app theme setup with colors and text styles
    - Add app_theme.dart for theme configuration
    - Add app_colors.dart for color definitions
    - Add app_text_styles.dart for text style definitions and theme.dart to export the theme files
    - Create theme folder structure under lib/core/

---
+++ lib/core/theme/app_text_theme.dart
@@ -0,0 +1,99 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'color_styles.dart';
+
+final TextTheme appTextTheme = TextTheme(
+  displayLarge: GoogleFonts.ibmPlexMono(
+    fontSize: 57,
+    fontWeight: FontWeight.w500,
+    height: 64 / 57,
+    letterSpacing: -0.25,
+  ),
+  displayMedium: GoogleFonts.inter(
+    fontSize: 45,
+    fontWeight: FontWeight.w500,
+    height: 52 / 45,
+  ),
+  displaySmall: GoogleFonts.inter(
+    fontSize: 36,
+    fontWeight: FontWeight.w500,
+    height: 44 / 36,
+  ),
+  headlineLarge: GoogleFonts.inter(
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+    height: 40 / 32,
+  ),
+  headlineMedium: GoogleFonts.inter(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    height: 36 / 28,
+  ),
+  headlineSmall: GoogleFonts.inter(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    height: 32 / 24,
+  ),
+  titleLarge: GoogleFonts.inter(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    height: 28 / 22,
+  ),
+  titleMedium: GoogleFonts.inter(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 24 / 16,
+    letterSpacing: 0.15,
+  ),
+  titleSmall: GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 20 / 14,
+    letterSpacing: 0.1,
+  ),
+  labelLarge: GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 20 / 14,
+    letterSpacing: 0.1,
+  ),
+  labelMedium: GoogleFonts.inter(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 16 / 12,
+    letterSpacing: 0.5,
+  ),
+  labelSmall: GoogleFonts.inter(
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    height: 16 / 11,
+    letterSpacing: 0.5,
+  ),
+  bodyLarge: GoogleFonts.inter(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 24 / 16,
+    letterSpacing: 0.15,
+  ),
+  bodyMedium: GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 20 / 14,
+    letterSpacing: 0.25,
+  ),
+  bodySmall: GoogleFonts.inter(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 16 / 12,
+    letterSpacing: 0.4,
+  ),
+);
+final TextStyle monoTextStyle = GoogleFonts.ibmPlexMono( fontWeight: FontWeight.
+w400, letterSpacing: 0.0,
+);
+
+
+
+
+
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,5 @@
+//lib/theme/core/theme.dart
+
+export 'color_styles.dart';
+export 'app_text_theme.dart';
+export 'app_theme.dart';
